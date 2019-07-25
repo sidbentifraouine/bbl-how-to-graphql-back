d9bf3bfab252750c7bff09b023e6f24504681269
Sid Bentifraouine <sidbentifraouine@gmail.com>
2019-07-25 15:50:23 +0200

    config(db): prisma

---
+++ src/generated/prisma-client/index.js
@@ -0,0 +1,17 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var prisma_lib_1 = require("prisma-client-lib");
+var typeDefs = require("./prisma-schema").typeDefs;
+
+var models = [
+  {
+    name: "Memory",
+    embedded: false
+  }
+];
+exports.Prisma = prisma_lib_1.makePrismaClientClass({
+  typeDefs,
+  models,
+  endpoint: `https://eu1.prisma.sh/sid-bentifraouine-e86c22/bbl-how-to-graphql-back/dev`
+});
+exports.prisma = new exports.Prisma();
